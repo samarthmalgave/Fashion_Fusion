2f83212735a1c7589d590455af6c37e9227a0077
samarth <sam@gmail.com>
2025-02-12 13:49:57 +0530

    Fashion Fusion

---
+++ online-shopping-frontend-master/online-shopping-frontend-master/src/pages/AboutUs.jsx
@@ -0,0 +1,32 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      Why Choose Us?<br></br>
+     ✔ Premium Quality – We source the finest fabrics to ensure durability and comfort.<br></br>
+     ✔ Trendy & Timeless Designs – Stay ahead of the fashion game with our curated collections.<br></br>
+     ✔ Perfect Fit for Every Body Type – A wide range of sizes designed for the modern man.<br></br>
+     ✔ Affordable Luxury – Look your best without breaking the bank.<br></br>
+
+        <br />
+        <br />
+        From classic formal wear to trendy street style, our collection includes:<br></br>
+        👕 Stylish T-Shirts & Shirts<br></br>
+        👖 Comfortable Jeans & Trousers<br></br>
+        🧥 Elegant Jackets & Outerwear<br></br>
+        🎽 Activewear for the Fitness Enthusiast<br></br>
+
+        <br />
+        <br />
+
+        HOMEGROWN INDIAN BRAND
+        
+        
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ online-shopping-frontend-master/online-shopping-frontend-master/src/pages/ContactUs.jsx
@@ -0,0 +1,19 @@
+
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      📍 Contact Information<br></br>
+      📧 Email: support@fashionfusion.com<br></br>
+      📞 Phone: +91 7517339059<br></br>
+      📍 Address: IACSD Akurdi.<br></br>
+
+        <br />
+        <br />
+        
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
